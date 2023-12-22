61088fcb27ff921f13db7eccb33e3398587cf1cb
lucidrains <lucidrains@gmail.com>
2023-12-22 10:07:30 -0800

    add slicing at specific dim, and pad_or_slice

--- pytorch_custom_utils/utils.py
+++ pytorch_custom_utils/utils.py
@@ -1,6 +1,23 @@
+from typing import Tuple
 import torch.nn.functional as F
 
-def pad_at_dim(t, pad, dim = -1, value = 0.):
+def pad_at_dim(t, pad: Tuple[int, int], *, dim = -1, value = 0.):
     dims_from_right = (- dim - 1) if dim < 0 else (t.ndim - dim - 1)
     zeros = ((0, 0) * dims_from_right)
     return F.pad(t, (*zeros, *pad), value = value)
+
+def slice_at_dim(t, dim_slice: slice, *, dim):
+    dim += (t.ndim if dim < 0 else 0)
+    colons = [slice(None)] * t.ndim
+    colons[dim] = dim_slice
+    return t[tuple(colons)]
+
+def pad_or_slice_to(t, length, *, dim, pad_value = 0):
+    curr_length = t.shape[dim]
+
+    if curr_length < length:
+        t = pad_at_dim(t, (0, length - curr_length), dim = dim, value = pad_value)
+    elif curr_length > length:
+        t = slice_at_dim(t, slice(0, length), dim = dim)
+
+    return t
