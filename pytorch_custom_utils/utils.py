4c8d263a19cf775e7cedb2fbbe6b1c0abff1a276
lucidrains <lucidrains@gmail.com>
2023-12-22 09:21:23 -0800

    start populating some padding / slicing related utility fns

---
+++ pytorch_custom_utils/utils.py
@@ -0,0 +1,6 @@
+import torch.nn.functional as F
+
+def pad_at_dim(t, pad, dim = -1, value = 0.):
+    dims_from_right = (- dim - 1) if dim < 0 else (t.ndim - dim - 1)
+    zeros = ((0, 0) * dims_from_right)
+    return F.pad(t, (*zeros, *pad), value = value)
