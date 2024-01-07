3367cb8db3f3669b766b2087b08f7a014248d646
lucidrains <lucidrains@gmail.com>
2024-01-07 10:58:25 -0800

    decorator that automatically unwraps a model with accelerator instance in a trainer class

--- pytorch_custom_utils/__init__.py
+++ pytorch_custom_utils/__init__.py
@@ -7,4 +7,7 @@ from pytorch_custom_utils.save_load import save_load
 from pytorch_custom_utils.get_adam_optimizer import get_adam_optimizer
 from pytorch_custom_utils.optimizer_scheduler_warmup import OptimizerWithWarmupSchedule
 
-from pytorch_custom_utils.accelerate_utils import add_wandb_tracker_contextmanager
+from pytorch_custom_utils.accelerate_utils import (
+    add_wandb_tracker_contextmanager,
+    auto_unwrap_model
+)
--- pytorch_custom_utils/accelerate_utils.py
+++ pytorch_custom_utils/accelerate_utils.py
@@ -56,3 +56,41 @@ def add_wandb_tracker_contextmanager(
         return klass
 
     return decorator
+
+# automatically unwrap model when attribute cannot be found on the maybe ddp wrapped main model
+
+class ForwardingWrapper:
+  def __init__(self, parent, child):
+    self.parent = parent
+    self.child = child
+
+  def __getattr__(self, key):
+    if hasattr(self.parent, key):
+      return getattr(self.parent, key)
+
+    return getattr(self.child, key)
+
+  def __call__(self, *args, **kwargs):
+    call_fn = self.__getattr__('__call__')
+    return call_fn(*args, **kwargs)
+
+def auto_unwrap_model(
+    accelerator_instance_name = 'accelerator',
+    model_instance_name = 'model'
+):
+    def decorator(klass):
+        _orig_init = klass.__init__
+
+        def __init__(self, *args, **kwargs):
+            _orig_init(self, *args, **kwargs)
+            model = getattr(self, model_instance_name)
+            accelerator = getattr(self, accelerator_instance_name)
+
+            assert isinstance(accelerator, Accelerator)
+            forward_wrapped_model = ForwardingWrapper(model, accelerator.unwrap_model(model))
+            setattr(self, model_instance_name, forward_wrapped_model)
+
+        klass.__init__ = __init__
+        return klass
+
+    return decorator
